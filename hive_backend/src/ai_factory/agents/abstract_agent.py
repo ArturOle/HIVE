28968639dbe9f17399070a36d253abef962b1a22
ArturOle <artur.oleksiski99@gmail.com>
2026-06-13 17:59:51 +0200

    Clean-up for prototype phase

--- hive_backend/src/ai_factory/agents/abstract_agent.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-
-class AbstractAgent(ABC):
-    def _describe():
-        """Method describing the agent"""
-
-    def _invoke():
-        """Method for agent workflow invocation"""
